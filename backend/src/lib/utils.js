714d6cb5b89f346de607de9a152d1f70c87af80f
Sayan Rajak Das <168341321+Sayan-Rajak-Das@users.noreply.github.com>
2025-01-22 23:55:44 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
         httpOnly: true, // Prevents XSS attacks
         sameSite: "strict", // Prevents CSRF
-        secure: process.env.NODE_ENV !== "development", // Use HTTPS in production
+        secure: process.env.NODE_ENV === "production", // Use HTTPS in production
     });
 
     return token;
