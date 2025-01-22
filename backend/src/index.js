942a9a62b08f1dd3e7195f60ac77c58b2c2b3dfa
Sayan Rajak Das <168341321+Sayan-Rajak-Das@users.noreply.github.com>
2025-01-22 19:15:37 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,10 +14,9 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true 
-}));
+    credentials : true,
+    origin : "*"
+}))
 app.use(express.json());
 app.use(cookieParser()); 
 
